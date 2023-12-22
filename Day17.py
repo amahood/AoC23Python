53cea9ef487d79b19df46018518d36155f3a334b
Adamn Mahood <adam.m.mahood@gmail.com>
2023-12-22 10:40:39 -0800

    Both samples work for Pt 2, trying real input

--- Day17.py
+++ Day17.py
@@ -1,47 +1,62 @@
 import queue
 
 def find_neighbors(row, col, numrows, numcols, last_ten):
-    
     neighbs = []
 
     has_gone_ten = False
     has_gone_four = True
 
+    #See if we've gone ten in a row and need to divert
     if last_ten.count("Up") == 10 or last_ten.count("Down") == 10 or last_ten.count("Right") == 10 or last_ten.count("Left") ==10:
         has_gone_ten = True
 
+    #See if we've met minimum condition of going four in a row
     last_direction = last_ten[9]
     if last_ten[8] != last_direction or last_ten[7] != last_direction or last_ten[6] != last_direction:
         has_gone_four = False
 
-    if has_gone_four == False:
-        if last_direction == "Up":
+    #Find out how many spaces we need left between us and the edge to make sure we can still go four (note I'm not taking into account multi-dimensional )
+    same_dir_counter = 0
+    i = 9
+    move = last_direction
+    while move == last_direction and i >= 0:
+        same_dir_counter += 1
+        move = last_ten[i]
+        i -= 1
+    
+    moves_left_to_four = 4 - same_dir_counter
+    moves_left_to_ten = 10 - same_dir_counter
+
+    #If we haven't gone four, then we can only go in the same direction, that is the only valid neighbor
+    if has_gone_four == False and last_direction != "Self":
+        if last_direction == "Up" and row >0:
             neighbs.append((row-1, col))
-        if last_direction == "Down":
+        if last_direction == "Down" and row < numrows-1:
             neighbs.append((row+1, col))
-        if last_direction == "Right":
+        if last_direction == "Right" and col < numcols-1:
             neighbs.append((row, col+1))
-        if last_direction == "Left":
+        if last_direction == "Left" and col > 0:
             neighbs.append((row, col-1))
+
     else:
-        #TODO WIP - redo the logic form last_three below
-        if row > 0 and not (has_gone_ten == True and last_ten[9] == "Up") and last_ten[9] != "Down":
+        if row > 0 and (not (has_gone_ten == True and last_direction == "Up") and last_direction != "Down"):
             neighbs.append((row-1, col))
 
-        #find same row neighbors
-        if col > 0 and not(has_gone_ten == True  and last_ten[9] == "Left") and last_ten[9] != "Right":
+        #See if left neighbor is valid
+        if col > 0 and (not (has_gone_ten == True  and last_direction == "Left") and last_direction != "Right"):
             neighbs.append((row,col-1))
-        if col < numcols-1 and not(has_gone_ten == True  and last_ten[9] == "Right") and last_ten[9] != "Left":
+        #SEe if right neighbor is valid
+        if col < numcols-1 and not (has_gone_ten == True  and last_direction == "Right") and last_direction != "Left":
             neighbs.append((row, col+1))
 
-        #find lower row neighbors
-        if row < numrows-1 and not(has_gone_ten == True  and last_ten[9] == "Down") and last_ten[9] != "Up":
+        #See if lower neighbor is valid
+        if row < numrows-1 and not (has_gone_ten == True  and last_direction == "Down") and last_direction != "Up":
             neighbs.append((row+1, col))
         
     return neighbs
 
-#f = open("Day17Input.txt")
-f = open("Day17TestInput.txt")
+f = open("Day17Input.txt")
+#f = open("Day17TestInput.txt")
 #f = open("Day17TestInput2.txt")
 VERYLARGENUMBER = 100000000000000000000
 
@@ -69,7 +84,7 @@ while leadingedge.empty() == False:
     current_location = leadingedge.get()
     #print("Visiting Location: " + str(current_location[1]))
 
-    if current_location[1] == (number_of_rows-1,number_of_columns-1):
+    if current_location[1] == (number_of_rows-1,number_of_columns-1) and (current_location[2][9] == current_location [2][8] == current_location[2][7] == current_location[2][6]):
        break
 
     #get neighbors
