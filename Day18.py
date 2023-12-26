a791b28b5a78e2a6cc76fed1e876810fb198e80e
Adamn Mahood <adam.m.mahood@gmail.com>
2023-12-25 16:01:55 -0800

    Refactored working PArt 1 (Day 18)

--- Day18.py
+++ Day18.py
@@ -16,8 +16,28 @@ for l in f:
     temp_instruction = digging_instruction(instruction[0], instruction[1], instruction[2][2:8])
     instruction_set.append(temp_instruction)
 
+"""
+#PArt 2 need to reparwsew instruction set
+for i in instruction_set:
+    #convert i.color to decimal and set to i.spaces
+    i.spaces = int(i.color[0:5], 16)
+    if i.color[5] == '0':
+        i.direction = "R"
+        i.color = ''
+    elif i.color[5] == '1':
+        i.direction = "D"
+        i.color = ''
+    elif i.color[5] == '2':
+        i.direction = "L"
+        i.color = ''
+    elif i.color[5] == '3':
+        i.direction = "U"
+        i.color = ''
+"""
+        
 print("Instructions loaded.")
 
+dig_map_not_normalized = set()
 dig_map = set()
 
 current_row = 0
@@ -27,41 +47,39 @@ for i in instruction_set:
     if i.direction == "R":
         for j in range(current_col, current_col+int(i.spaces)):
             current_col = current_col + 1
-            dig_map.add(((current_row,current_col), i.color))
+            dig_map_not_normalized.add((current_row,current_col))
             
     elif i.direction == "L":
         for j in range(current_col, current_col+int(i.spaces)):
             current_col = current_col - 1
-            dig_map.add(((current_row,current_col), i.color))
+            dig_map_not_normalized.add((current_row,current_col))
             
     elif i.direction == "U":
         for j in range(current_row, current_row+int(i.spaces)):
             current_row = current_row - 1
-            dig_map.add(((current_row,current_col), i.color))
+            dig_map_not_normalized.add((current_row,current_col))
 
     elif i.direction == "D":
         for j in range(current_row, current_row + int(i.spaces)):
             current_row = current_row + 1
-            dig_map.add(((current_row,current_col), i.color))
-
+            dig_map_not_normalized.add((current_row,current_col))
 
 #find the max row and col in dig_map
 max_row = 0
 max_col = 0
-for i in dig_map:
-    if i[0][0] > max_row:
-        max_row = i[0][0]
-    if i[0][1] > max_col:
-        max_col = i[0][1]
-
-#find the min row and col in dig_map
 min_row = 0
 min_col = 0
-for i in dig_map:
-    if i[0][0] < min_row:
-        min_row = i[0][0]
-    if i[0][1] < min_col:
-        min_col = i[0][1]
+for i in dig_map_not_normalized:
+    if i[0] > max_row:
+        max_row = i[0]
+    if i[1] > max_col:
+        max_col = i[1]
+    if i[0] < min_row:
+        min_row = i[0]
+    if i[1] < min_col:
+        min_col = i[1]
+
+#find the min row and col in dig_map
 
 print("Max row: " + str(max_row))
 print("Max col: " + str(max_col))
@@ -74,16 +92,24 @@ cols_to_add = abs(min_col)
 total_rows = max_row + rows_to_add
 total_cols = max_col + cols_to_add
 
+#initialize full map to . - this seems inefficient - CAN I TRY THIS AS A SET? - Already have this 
+"""
 dig_map_list = []
 for i in range(total_rows+1):
     temp_dig_row = []
     for j in range(total_cols+1):
         temp_dig_row.append('.')
     dig_map_list.append(temp_dig_row)
+"""
+    
+#REminder - dig map is the set of tuples of locaton and character on the path
+    #This is shifting them all to nromalize to 0 and then setting them to the char and adding to set
+for i in dig_map_not_normalized:
+    temp_location = (i[0] + rows_to_add, i[1] + cols_to_add)
+    dig_map.add(temp_location)
 
-for i in dig_map:
-    dig_map_list[i[0][0] + rows_to_add][i[0][1]+cols_to_add] = i[1]
 
+"""
 temp_string_to_add_top_bottom = []
 for i in range(total_cols+1):
     temp_string_to_add_top_bottom.append('.')
@@ -95,45 +121,39 @@ for r in dig_map_list:
     r.append('.')
 
 print("Map created.")
+"""
 
 pts_to_check = set()
 vistied_pts = set()
-pts_to_check.add((0,0))
-pts_to_check.add((0,len(dig_map_list[0])-1))
-pts_to_check.add((len(dig_map_list)-1,0))
-pts_to_check.add((len(dig_map_list)-1,len(dig_map_list[0])-1))
+pts_outside_map = set()
+pts_to_check.add((-1,-1))
+pts_to_check.add((-1,total_cols+1))
+pts_to_check.add((total_rows+1,-1))
+pts_to_check.add((total_rows+1,total_cols+1))
+
+length = total_rows + 3
+width = total_cols + 3
 
 while len(pts_to_check) > 0:
     pt = pts_to_check.pop()
     if pt not in vistied_pts:
         vistied_pts.add(pt)
-        #COP GENERATED BELOW HERE, NEED TO CHECK
-        if dig_map_list[pt[0]][pt[1]] == '.':
-            dig_map_list[pt[0]][pt[1]] = 'X'
-            if pt[0] > 0:
+        if pt not in dig_map:
+            pts_outside_map.add(pt)
+            if pt[0] > -1:
                 pts_to_check.add((pt[0]-1, pt[1]))
-            if pt[0] < len(dig_map_list)-1:
+            if pt[0] < total_rows+1:
                 pts_to_check.add((pt[0]+1, pt[1]))
-            if pt[1] > 0:
+            if pt[1] > -1:
                 pts_to_check.add((pt[0], pt[1]-1))
-            if pt[1] < len(dig_map_list[0])-1:
+            if pt[1] < total_cols+1:
                 pts_to_check.add((pt[0], pt[1]+1))
+print("Dimensions - " + str(length*width))
+print("Visited points - " + str(len(vistied_pts)))
+print("Dig points - " + str(len(dig_map)))
+print("Outside points - " + str(len(pts_outside_map)))
 
-running_sum = 0
-for r in dig_map_list:
-    string_to_print = ""
-    for c in r:
-        if c == '.':
-            string_to_print += '.'
-            running_sum += 1
-        elif c == 'X':
-            string_to_print += 'X'
-        else:
-            string_to_print += '#'
-            running_sum += 1
-    print(string_to_print)
-
-print("Running sum: " + str(running_sum))
+print("Total dug area - " + str(length*width - len(pts_outside_map)))
 
 
     
