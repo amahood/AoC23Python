63c88ccbb8a03b05ebeb9b9ee72f0030cb207eae
Adam Mahood <adam.m.mahood#gmail.com>
2023-12-27 16:02:30 -0800

    Pt. 2 working solution for sample input

--- Day19.py
+++ Day19.py
@@ -6,6 +6,7 @@ class workflow_rule:
         self.condition = condition
         self.value = value
         self.next_workflow = next_workflow
+        self.pt2result = 'F'
 
 class workflow:
     def __init__(self, label, rules):
@@ -151,36 +152,123 @@ for p in parts:
 
 print(str(running_sum))
 
+
 def find_paths_to_a(input_workflow, rules_so_far, workflow_set):
     paths_to_a = []
     local_rsf = copy.deepcopy(rules_so_far)
 
-    #parse workflow rules
-    #BUG - But not sure it matters, if a workflow only has rules that end in A, you will get paths for each rule of that node (the lnx bug)
-    print("at " + input_workflow.label)
     for r in input_workflow.rules:
         if r.next_workflow == 'A':
-            #new_path = copy.deepcopy(local_rsf)
-            #new_path.append(r)
+            if r.letter != "def":
+                local_rsf_success = copy.deepcopy(local_rsf)
+                #append failure to main tracker
+                failure_rule = copy.deepcopy(r)
+                failure_rule.pt2result = 'F'
+                local_rsf.append(failure_rule)
+                #Append pass to path and return as path
+                success_rule = copy.deepcopy(r)
+                success_rule.pt2result = 'T'
+
+                local_rsf_success.append(success_rule)
+                paths_to_a.append(local_rsf_success)
+            if r.letter == "def":
+                #append pass to path and return as path
+                success_rule = copy.deepcopy(r)
+                success_rule.pt2result = 'T'
+                local_rsf_success = copy.deepcopy(local_rsf)
+                local_rsf_success.append(success_rule)
+                paths_to_a.append(local_rsf_success)
+        elif (r.next_workflow == 'R' and r.letter != "def"): #Handles conditions for R that we don't meet and move past
+            #Add to path but don't call find As?
+            r.pt2result = 'F'
             local_rsf.append(r)
-            paths_to_a.append(local_rsf)
-        elif r.next_workflow != 'R':   
-            #temp_path = copy.deepcopy(local_rsf)
-            #temp_path.append(r)
-            local_rsf.append(r)
+        elif r.next_workflow != 'R' and next_workflow != 'A': #handles workflows that go to different location BUG but we are always assumign they fail / tracking they fail
+            local_rsf_success = copy.deepcopy(local_rsf)
+            #append failure to main tracker
+            failure_rule = copy.deepcopy(r)
+            failure_rule.pt2result = 'F'
+            local_rsf.append(failure_rule)
+            #Append pass, find, and pass ot recusion
+            success_rule = copy.deepcopy(r)
+            success_rule.pt2result = 'T'
+
+            local_rsf_success.append(success_rule)
             nw = find_workflow(r.next_workflow, workflow_set)
-            next_layer_paths_to_a = find_paths_to_a(nw, local_rsf, workflow_set)
+            next_layer_paths_to_a = find_paths_to_a(nw, local_rsf_success, workflow_set)
             for path in next_layer_paths_to_a:
                 paths_to_a.append(path)
         
     return paths_to_a
 
+#PART 2 CORE LOGIC STARTS HERE
 empty_list = []
 paths_from_inpoint = find_paths_to_a(inpoint, empty_list, workflows)
 
-# I NOW HAVE ALL THE CONDITIONS BUT NOT WHETHER THEY PASSED OR FAILED TO GET TO THE NEXT STEP
 
+def process_path_for_ranges(path):
+    letter_ranges = []
+    x_range = range(1,4001)
+    m_range = range(1,4001)
+    a_range = range(1,4001)
+    s_range = set(range(1,4001))
 
+    #DO STUFF
+    condition_tracker = 0
+    for p in path:
+        if p.letter == 'x':
+            if p.condition == '<':
+                if p.pt2result == 'F':
+                    x_range =set(filter(lambda x: x>=int(p.value), x_range))
+                elif p.pt2result == 'T':
+                    x_range =set(filter(lambda x: x<int(p.value), x_range))
+            elif p.condition == '>':
+                if p.pt2result == 'F':
+                    x_range =set(filter(lambda x: x<=int(p.value), x_range))
+                elif p.pt2result == 'T':
+                    x_range =set(filter(lambda x: x>int(p.value), x_range))
+        elif p.letter == 'm':
+            if p.condition == '<':
+                if p.pt2result == 'F':
+                    m_range =set(filter(lambda x: x>=int(p.value), m_range))
+                elif p.pt2result == 'T':
+                    m_range =set(filter(lambda x: x<int(p.value), m_range))
+            elif p.condition == '>':
+                if p.pt2result == 'F':
+                    m_range =set(filter(lambda x: x<=int(p.value), m_range))
+                elif p.pt2result == 'T':
+                    m_range =set(filter(lambda x: x>int(p.value), m_range))
+        elif p.letter == 'a':
+            if p.condition == '<':
+                if p.pt2result == 'F':
+                    a_range =set(filter(lambda x: x>=int(p.value), a_range))
+                elif p.pt2result == 'T':
+                    a_range =set(filter(lambda x: x<int(p.value), a_range))
+            elif p.condition == '>':
+                if p.pt2result == 'F':
+                    a_range =set(filter(lambda x: x<=int(p.value), a_range))
+                elif p.pt2result == 'T':
+                    a_range =set(filter(lambda x: x>int(p.value), a_range))
+        elif p.letter == 's':
+            if p.condition == '<':
+                if p.pt2result == 'F':
+                    s_range =set(filter(lambda x: x>=int(p.value), s_range))
+                elif p.pt2result == 'T':
+                    s_range =set(filter(lambda x: x<int(p.value), s_range))
+            elif p.condition == '>':
+                if p.pt2result == 'F':
+                    s_range =set(filter(lambda x: x<=int(p.value), s_range))
+                elif p.pt2result == 'T':
+                    s_range =set(filter(lambda x: x>int(p.value), s_range))
+
+        condition_tracker += 1
+
+    letter_ranges.append(x_range)
+    letter_ranges.append(m_range)
+    letter_ranges.append(a_range)
+    letter_ranges.append(s_range)
+    return letter_ranges
+
+running_sum = 0
 for p in paths_from_inpoint:
     path = ""
     labels = ""
@@ -188,12 +276,26 @@ for p in paths_from_inpoint:
             labels = labels + " " + w.workflow_label
             if w.letter != "def":
                 rule_text = w.letter + w.condition + w.value
-                path = path + " " + rule_text
+                path = path + " " + rule_text + " " + w.pt2result
+            elif w.letter == "def" and w.next_workflow == 'A':
+                path = path + " def"
             
     print(labels)
     print(path)
 
-
-print("Hold")
+    #Start attempt at calculation here
+    #CANT ASSUME IT"S ONLY THE LAST THAT IS OPPOSITE< NEED TO TRACK ALONG THE WAY
+    # MAYBE HAVE PATHS RIGH TNOW< BUT NEED TO UPDATE THE RANG CALC FUNCTIONS TO UYSE TRUE/FALSE
+    output_ranges = process_path_for_ranges(p)
+    
+    length = ""
+    for r in output_ranges:
+        length = length + " " + str(len(r))
+    print(length)
+    combos_for_row = len(output_ranges[0])*len(output_ranges[1])*len(output_ranges[2])*len(output_ranges[3])
+    print(str(combos_for_row))
+    running_sum = running_sum + combos_for_row
+    print(" \n")
+print("running sum " + str(running_sum))
 
 
